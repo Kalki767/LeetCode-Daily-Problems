c027eb981349944b49972a25c6a287de038657ca
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2026-01-18 17:49:15 +0300

    Time: 1545 ms (97.62%) | Memory: 20.1 MB (10.71%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
@@ -1,20 +1,24 @@
 class Solution:
-    def largestSquareArea(
-        self, bottomLeft: List[List[int]], topRight: List[List[int]]
-    ) -> int:
-        max_size = 0
-
-        for (bottom_left_i, top_right_i), (
-            bottom_left_j,
-            top_right_j,
-        ) in combinations(zip(bottomLeft, topRight), 2):
-            w = min(top_right_i[0], top_right_j[0]) - max(
-                bottom_left_i[0], bottom_left_j[0]
-            )
-            h = min(top_right_i[1], top_right_j[1]) - max(
-                bottom_left_i[1], bottom_left_j[1]
-            )
-
-            max_size = max(max_size, min(w, h))
-
-        return max_size * max_size
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        merged = []
+        max_area = 0
+        n = len(bottomLeft)
+        for i in range(n):
+            merged.append([bottomLeft[i], topRight[i]])
+        merged.sort()
+        for i in range(n-1):
+            ix1, iy1 = merged[i][0]
+            ix2, iy2 = merged[i][1]
+            for j in range(i+1,n):
+                jx1, jy1 = merged[j][0]
+                jx2, jy2 = merged[j][1]
+                if ix2 <= jx1:
+                    break
+                if jy2 <= iy1 or jy1 >= iy2:
+                    continue
+                width = min(ix2,jx2) - jx1
+                height = min(iy2, jy2) - max(iy1, jy1)
+                side = min(width, height)
+                if side > 0:
+                    max_area = max(max_area, side ** 2)
+        return max_area
